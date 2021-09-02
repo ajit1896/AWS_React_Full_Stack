8a0edae7309a364bd93ae5f965dc82cfc7d5851b
Ajit Pal Jassal <ajit.jassal@ucalgary.ca>
2021-09-02 11:59:26 -0600

    Version V2

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bravo...!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Modify <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
